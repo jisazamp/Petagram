440b3631a19e7e45e6907f0295c6d202f62188d3
Juan P. Isaza <jisazamp@gmail.com>
2020-09-02 13:40:02 -0500

    added Snackbar to MainActivity

--- app/src/main/java/com/example/petagram/MainActivity.java
+++ app/src/main/java/com/example/petagram/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.petagram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,7 +24,16 @@ public class MainActivity extends AppCompatActivity {
         miFAB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                //Toast.makeText(getBaseContext(), getResources().getString(R.string.mensaje), Toast.LENGTH_SHORT).show();
+                Snackbar.make(view, getResources().getString(R.string.mensaje), Snackbar.LENGTH_LONG)
+                        .setAction(getResources().getString(R.string.texto_accion), new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Log.i("SNACKBAR", "Click en Snackbar");
+                            }
+                        })
+                        .setActionTextColor(getResources().getColor(R.color.colorPrimary))
+                        .show();
             }
         });
     }
